3b4c577af88dc06f26d7d36c63916368a1ee682c
Nagy János <nagy@zold.lan>
2024-05-15 12:28:21 +0200

    Add project files.

---
+++ WpfAnime/ViewModel/MainViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using WpfAnime.Command;
+using WpfAnime.View;
+
+namespace WpfAnime.ViewModel
+{
+    public class MainViewModel:INotifyPropertyChanged
+    {
+		private object currentView;
+
+		public object CurrentView
+		{
+			get { return currentView; }
+			set { 
+				currentView = value;
+				OnPropertyChanged();
+			}
+		}
+
+		HomeView homeView;
+		UsersView usersView;
+
+        public event PropertyChangedEventHandler? PropertyChanged;		
+
+        public RelayCommand openHome { get; }
+		public RelayCommand openUsers { get; } 
+
+        public MainViewModel()
+        {
+			homeView = new HomeView();
+			usersView = new UsersView();
+
+			openHome = new RelayCommand(X=> CurrentView=homeView);
+			openUsers = new RelayCommand(X=> CurrentView=usersView);
+
+			CurrentView = homeView;
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
